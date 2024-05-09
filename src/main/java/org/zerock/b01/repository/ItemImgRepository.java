53f185855f0c8dd7733267cc8cf5abf961c7a27c
10-5\kkw <jhchoi524@gmail.com>
2024-05-09 19:51:27 +0900

    0509 장바구니 백 db 입력값 확인 프론트 연결중

---
+++ src/main/java/org/zerock/b01/repository/ItemImgRepository.java
@@ -0,0 +1,10 @@
+package org.zerock.b01.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.zerock.b01.domain.ItemImg;
+import java.util.List;
+
+public interface ItemImgRepository extends JpaRepository<ItemImg, Long> {
+
+
+}
