3a5c8b46ac608efe4e7d414484f76c0be04b8c7c
10-5\kkw <jhchoi524@gmail.com>
2024-05-10 17:45:00 +0900

    05-10 머지 전 커밋

---
+++ src/main/java/org/zerock/b01/repository/ItemImgRepository.java
@@ -0,0 +1,10 @@
+package org.zerock.b01.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.zerock.b01.domain.ItemImage;
+
+
+public interface ItemImgRepository extends JpaRepository<ItemImage, String> {
+
+
+}
