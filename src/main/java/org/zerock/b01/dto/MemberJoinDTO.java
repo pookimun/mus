e821e06470c4cc7ca17ecccf4ac9f5dd23810e4e
10-5\kkw <nanabad@naver.com>
2024-05-10 13:20:03 +0900

    완벽한,,, 회원가입 구현,,, 후후후

--- src/main/java/org/zerock/b01/dto/MemberJoinDTO.java
+++ src/main/java/org/zerock/b01/dto/MemberJoinDTO.java
@@ -1,10 +1,16 @@
 package org.zerock.b01.dto;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 @Data
 public class MemberJoinDTO {
     private String mid;
+
+    @NotBlank(message = "이메일을 입력해 주세요")
     private String m_pw;
+
+    @NotBlank(message = "비밀번호를 입력해 주세요")
     private String m_email;
     private boolean m_del;
     private boolean m_social;
