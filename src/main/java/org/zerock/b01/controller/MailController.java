db2a1c82366382978a0b4c13e5fe06dd2f8f82f1
pookimun <mazzant248@gmail.com>
2024-05-12 17:45:59 +0900

    인증번호 받기 완료

--- src/main/java/org/zerock/b01/controller/MailController.java
+++ src/main/java/org/zerock/b01/controller/MailController.java
@@ -7,9 +7,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
+import org.zerock.b01.dto.AddressDTO;
 import org.zerock.b01.dto.EmailCheckDTO;
+import org.zerock.b01.dto.MemberDTO;
 import org.zerock.b01.service.MailSendService;
+import org.zerock.b01.service.MemberService;
 
 @Log4j2
 @RestController
@@ -17,25 +21,30 @@ import org.zerock.b01.service.MailSendService;
 @RequestMapping("/member/forgot")
 public class MailController {
     private final MailSendService mailService;
+    private final MemberService memberService;
 
     @PostMapping("/mailSend")
-    public ModelAndView mailSend(@RequestParam String m_email) {
+    public ModelAndView mailSend(@RequestParam String m_email, RedirectAttributes redirectAttributes) {
         try {
             mailService.joinEmail(m_email);
+            redirectAttributes.addFlashAttribute("인증 번호 전송!");
             return new ModelAndView("redirect:/member/forgot");
         } catch (Exception e) {
             e.printStackTrace();
+            redirectAttributes.addFlashAttribute("인증 번호 전송 실패");
             return new ModelAndView("redirect:/member/forgot");
         }
     }
 
     @PostMapping("/mailAuthCheck")
-    public String AuthCheck(@RequestParam @Valid EmailCheckDTO emailCheckDTO) {
-        Boolean Checked = mailService.CheckAuthNum(emailCheckDTO.getM_email(), emailCheckDTO.getAuthNum());
+    public ModelAndView AuthCheck(@RequestParam String authNum, RedirectAttributes redirectAttributes) {
+        Boolean Checked = mailService.CheckAuthNum(authNum);
         if (Checked) {
-            return "ok";
+            redirectAttributes.addFlashAttribute("인증 번호 일치");
+            return new ModelAndView("redirect:/member/changePw");
         } else {
-            throw new NullPointerException("뭔가 잘못!");
+            redirectAttributes.addFlashAttribute("인증 번호 불일치");
+            return new ModelAndView("redirect:/member/forgot");
         }
     }
 }
