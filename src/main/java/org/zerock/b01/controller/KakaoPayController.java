33135303a7673561768d90cd72fe61cd536a7f0a
dooboooooo <tjddms001014@naver.com>
2024-05-15 18:05:42 +0900

    멤버 포인트 연결 완료

--- src/main/java/org/zerock/b01/controller/KakaoPayController.java
+++ src/main/java/org/zerock/b01/controller/KakaoPayController.java
@@ -1,23 +1,13 @@
 package org.zerock.b01.controller;
 
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.validation.BindException;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
-import org.zerock.b01.domain.Orders;
 import org.zerock.b01.dto.*;
-import org.zerock.b01.service.AddressService;
-import org.zerock.b01.service.ItemService;
-import org.zerock.b01.service.KakaoPayService;
-import org.zerock.b01.service.OrdersService;
+import org.zerock.b01.service.*;
 
 import java.security.Principal;
 import java.util.ArrayList;
@@ -32,6 +22,8 @@ public class KakaoPayController {
     private final AddressService addressService;
     private final OrdersService ordersService;
     private final ItemService itemService;
+    private final CartService cartService;
+    private final MemberService memberService;
 
 
     // 결제 요청
@@ -64,6 +56,7 @@ public class KakaoPayController {
                     .od_size(ordersDTO.getSizes()[i])
                     .od_color(ordersDTO.getColors()[i])
                     .od_price(itemService.readOne(inos[i]).getI_price()) // 구매당시 가격저장
+                    .cdid(ordersDTO.getCdids()[i])
                     .build();
             log.info("ordersDetailDTO : " + ordersDetailDTO);
             ordersDetailDTOList.add(ordersDetailDTO);
@@ -81,8 +74,30 @@ public class KakaoPayController {
         log.info("결제 성공 컨트롤러 실행 ... ono : " + ono);
         // 결제 성공 시 orders, ordersDetail 테이블에 데이터 저장하기
         KakaoApproveResponseDTO kakaoApprove = kakaoPayService.approveResponse(pgToken, ono);
+        log.info("kakaoApprove : " + kakaoApprove);
         ordersService.paymentSuccess(ono); // 결제성공으로 update
-        log.info("완료");
+
+        // 멤버 포인트 변화 금액
+        int point = 0; // 초기값 지정
+
+        OrdersListDTO ordersListDTO = ordersService.readOne(ono);
+        for (OrdersDetailDTO ordersDetailDTO : ordersListDTO.getOrdersDetailDTOList()){
+            // 결제완료 된 CartDetail의 paymentSuccess 필드 값 1(결제완료)로 수정
+            cartService.cdidPaymentSuccess(ordersDetailDTO.getCdid());
+            if(ordersListDTO.getPointFirstUse() == 0) {
+                // 적립금 선할인을 사용하지 않았을 때에만 상품 예상 적립금 point 변수에 저장
+                point += (ordersDetailDTO.getOd_count() * ordersDetailDTO.getOd_price() * 0.005); // 적립예정 포인트 누적저장
+            }
+        }
+        log.info("상품들의 적립금액 : " + point);
+
+        // 포인트 사용값 만큼 보유 보인트에서 빼기
+        point -= ordersListDTO.getPointUse();
+        log.info("멤버 포인트 최종 변화 금액 : " + point);
+
+        // 포인트 업뎃
+        memberService.changePoint(kakaoApprove.getPartner_user_id(), point);
+
         // 리다이렉션을 위한 RedirectView 생성
         RedirectView redirectView = new RedirectView();
         redirectView.setUrl("/orders/success"); // 리다이렉션할 URL 지정
