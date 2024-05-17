8250502bae371963ec08cf0cec0d4223b7ee7a2d
10-5\kkw <tjddms001014@naver.com>
2024-05-17 14:46:43 +0900

    cartController 수정중 ...

--- src/main/java/org/zerock/b01/dto/CartDTO.java
+++ src/main/java/org/zerock/b01/dto/CartDTO.java
@@ -1,24 +1,26 @@
 package org.zerock.b01.dto;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-@Setter
-@Getter
+@Data
+@Builder
+@NoArgsConstructor
 public class CartDTO {
 
     private Long cartItemId; // 장바구니 상품 아이디
     private String itemNm; // 상품명
     private int price; // 상품 금액
-    //private int count; // 수량
-    private String imgUrl; // 상품 이미지 경로
+    private int count; // 수량
+    private String fileName;
+    private String uuid;
 
-    public CartDTO(Long cartItemId, String itemNm, int price, int count, String imgUrl) {
+    public CartDTO(Long cartItemId, String itemNm, int price, int count, String fileName, String uuid) {
         this.cartItemId = cartItemId;
         this.itemNm = itemNm;
         this.price = price;
-        //this.count = count;
-        this.imgUrl = imgUrl;
+        this.count = count;
+        this.fileName = fileName;
+        this.uuid = uuid;
     }
 
 }
--- src/main/java/org/zerock/b01/repository/CartDetailRepository.java
+++ src/main/java/org/zerock/b01/repository/CartDetailRepository.java
@@ -13,17 +13,14 @@ public interface CartDetailRepository extends JpaRepository<CartDetail, Long> {
 
     CartDetail findByCart_CnoAndItem_Ino(Long cno, Long itemId);
 
-    @Query("select new org.zerock.b01.dto.CartDTO(ci.cdid, i.i_name, i.i_price, ci.count, im.imgUrl) " +
-            "from CartDetail ci, ItemImg im " +
-            "join ci.item i " +
-            "where ci.cart.cno = :cartId " +
-            "and im.item.ino = ci.item.ino " +
-            "and im.repImgYn = 'Y' " +
-            "order by ci.regDate desc")
-    /* new com.ogu.ogoods.dto.CartDetailDto
-    DTO를 직접 조회하기 위해서 new를 통해 생성자를 통해 객체 반환
-    JPQL에서는 패키지와 함께 클래스명을 작성해줘야 함
-    생성자 파라미터 순서는 DTO 클래스 명시 순으로 넣을 것 */
+    @Query("select new org.zerock.b01.dto.CartDTO(cd.cdid, i.i_name, i.i_price, cd.count, im.fileName, im.uuid) " +
+            "from CartDetail cd " +
+            "join cd.item i " +
+            "join ItemImage im on im.item.ino = i.ino " +
+            "where cd.cart.cno = :cartId " +
+            "group by i.ino " +
+            //"HAVING im.ord = 0 " +
+            "order by cd.regDate desc")
     List<CartDTO> findCartDetailDtoList(Long cartId);
 
     @Query("select cd from CartDetail cd where cd.cart = :cart and cd.paymentSuccess = 0")
